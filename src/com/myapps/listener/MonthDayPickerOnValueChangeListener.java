2f5deb1ba0258493b76631f893cdaece14ce33c5
Kenta Miyahara <miyahara@mitene.ad.jp>
2014-11-28 17:22:35 +0900

    選択日付の表示を変更
    選択した日付の表示に対して
    ゼロ詰め処理を適用

--- src/com/myapps/listener/MonthDayPickerOnValueChangeListener.java
+++ src/com/myapps/listener/MonthDayPickerOnValueChangeListener.java
@@ -21,13 +21,13 @@ public class MonthDayPickerOnValueChangeListener implements NumberPicker.OnValue
 		switch ( picker.getId() ) {
 			case R.id.monthPickerView : {
 				Config.DebugLog( "MonthDayPickerFragment >>> onValueChange >>> Month" );
-				this._monthDayPickerFragment.setSelectMonth( String.valueOf( newVal ) );
+				this._monthDayPickerFragment.setSelectMonth( String.format( "%02d" , newVal ) );
 				this._monthDayPickerFragment.getSelectMonthTextView().setText( String.format( "%s月" , this._monthDayPickerFragment.getSelectMonth() ) );
 				break;
 			}
 			case R.id.dayPickerView : {
 				Config.DebugLog( "MonthDayPickerFragment >>> onValueChange >>> Day" );
-				this._monthDayPickerFragment.setSelectDay( String.valueOf( newVal ) );
+				this._monthDayPickerFragment.setSelectDay( String.format( "%02d" , newVal ) );
 				this._monthDayPickerFragment.getSelectDayTextView().setText( String.format( "%s日" , this._monthDayPickerFragment.getSelectDay() ) );
 				break;
 			}
